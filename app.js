f7d8a17b7115ae36860c06623c254625572e202c
Charizard <root@charizard.icmc>
2017-07-10 19:37:12 -0300

    Ultima versao

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var app = express();
 app.use(bodyParser.json());
 
 
+var lastPing = 0;
 var status = null;
 function store(){
 	var url = 'mongodb://mongodb:27017/temgentenopet';
@@ -31,6 +32,7 @@ app.get('/qwertyuiop/sim', function (req, res) {
 		store();
 	}
 	res.send("ok");
+	lastPing = new Date();
 	console.log(status);
 });
 
@@ -40,41 +42,62 @@ app.get('/qwertyuiop/nao', function (req, res) {
 		store();
 	}
 	res.send("ok");
+	lastPing = new Date();
 	console.log(status);
 });
 
 app.get('/qwertyuiop/webhook', function (req, res) {
 	res.send("Tem gente no PET: "+ status || "Nao sei");
 });
+var subscribers = [];
+function sendMsg(msg){
+	var options = {
+		hostname: "api.telegram.org",
+		path: '/'+process.env.TELEGRAM_TOKEN+'/sendMessage',
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Content-Length": msg.length
+		}
+	}
+	var post_req = https.request(options, function (res) {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
+	});
 
+	post_req.on('error', function(e) {
+	    console.log('problem with request: ' + e.message);
+	});
+
+	post_req.write(msg);
+	post_req.end();
+}
 app.post('/qwertyuiop/webhook', function (req, res) {
 	console.log(req.body);
 	try {
+		var responseText = '';
+		if(req.body.message.text=="/subscribe"){
+			var idx = subscribers.indexOf(req.body.message.chat.id)
+			if(idx == -1){
+				subscribers.push(req.body.message.chat.id);
+			}
+			responseText = "Inscrito!";
+		} else if(req.body.message.text=="/unsubscribe"){
+			var idx = subscribers.indexOf(req.body.message.chat.id)
+			if(idx > -1){
+				subscribers.splice(idx, 1);
+			}
+			responseText = "Tchau!";
+		} else {
+			responseText = "Tem gente no PET: "+ (status || "Nao sei");
+			console.log(subscribers);
+		}
 		var msg = JSON.stringify({
 			"chat_id": req.body.message.chat.id,
-			"text": "Tem gente no PET: "+ status || "Nao sei",
+			"text": responseText,
 		});
 		res.json("ok");
-		var options = {
-			hostname: "api.telegram.org",
-			path: '/'+process.env.TELEGRAM_TOKEN+'/sendMessage',
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				"Content-Length": msg.length
-			}
-		}
-		var post_req = https.request(options, function (res) {
-			console.log('STATUS: ' + res.statusCode);
-			console.log('HEADERS: ' + JSON.stringify(res.headers));
-		});
-
-		post_req.on('error', function(e) {
-		    console.log('problem with request: ' + e.message);
-		});
-
-		post_req.write(msg);
-		post_req.end();
+		sendMsg(msg);
 	} catch(e) {
 		console.log(e);
 		res.json("ok");
@@ -84,4 +107,21 @@ app.post('/qwertyuiop/webhook', function (req, res) {
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
 });
-
+var delay = 3000;
+setInterval(function(){
+	if(lastPing == 0 || (new Date() - lastPing) < 30000){
+		delay = 5000;
+		return;
+	}
+	console.log("Cade??");
+	console.log(lastPing);
+	console.log(new Date());
+	subscribers.forEach(function(chatid){
+		var msg = JSON.stringify({
+			"chat_id": chatid,
+			"text": "Cade meu arduino????",
+		});
+		sendMsg(msg);
+	});
+	delay *= 5;
+}, 10000);
